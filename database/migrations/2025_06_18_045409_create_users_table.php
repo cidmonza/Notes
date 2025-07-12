7ad2e5f0874871bd3c5cabcb9bd7f9eb0f923e56
cidmonza <stinermonza@gmail.com>
2025-07-12 07:39:07 -0300

    configurações para rodar no linux

--- database/migrations/2025_06_18_045409_create_users_table.php
+++ database/migrations/2025_06_18_045409_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id()->autoIncrement();
             $table->string('username', 50)->nullable();
-            $table->string('password', 20)->nullable();
+            $table->string('password', 60)->nullable();
             $table->dateTime('last_login')->nullable();
             $table->timestamps();
             $table->softDeletes();
